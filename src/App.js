356946217235d1d89734faf1d6ce1065383856a3
Angelina <unliantazy@gmail.com>
2021-10-12 22:14:14 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import map from './images/moscow-tourist-map.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={map} alt=""/>
+        <h1>Our map</h1>
     </div>
   );
 }
