c8424f9b7c2956439c7a27716cca64ddc3f2d697
kupcova <kupcova@csmplanet.ru>
2021-10-19 14:30:01 +0300

    tabs icons + styles

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -1,10 +1,11 @@
 import React, {useCallback} from 'react';
 import {NavLink} from "react-router-dom";
-import FiltersIcon from '../images/filters.png'
-import LayersIcon from '../images/layers.png'
-import ServicesIcon from '../images/services.png'
-// import {ReactComponent as LayersIcon} from '../images/icons/layers.svg'
-// import {ReactComponent as ServicesIcon} from '../images/icons/services.svg'
+// import FiltersIcon from '../images/filters.png'
+// import LayersIcon from '../images/layers.png'
+// import ServicesIcon from '../images/services.png'
+import {ReactComponent as LayersIcon} from '../images/icons/layers.svg'
+import {ReactComponent as ServicesIcon} from '../images/icons/services.svg'
+import {ReactComponent as FiltersIcon} from '../images/icons/filters.svg'
 
 const Tabs = () => {
     const renderTab = useCallback((tab, text, icon) => (
@@ -17,7 +18,9 @@ const Tabs = () => {
                 activeStyle={{  }}
                 // style= {{ cursor: loading && 'wait' }}
             >
-                <span className={'tabs-img'}><img src={icon} alt=""/></span>
+                <span className={'tabs-img'}>
+                    {icon}
+                </span>
                 <span className={'d-flex align-items-center events-header__item--span'}>
                     {text}
                 </span>
@@ -27,9 +30,9 @@ const Tabs = () => {
 
     return (
         <header className="tabs-header">
-            {renderTab('', 'Фильтры', FiltersIcon)}
-            {renderTab("layers", 'Слои', LayersIcon)}
-            {renderTab("services", 'Инструменты', ServicesIcon)}
+            {renderTab('', 'Фильтры', <FiltersIcon />)}
+            {renderTab("layers", 'Слои', <LayersIcon style={{width: 15, height: 15}}/>)}
+            {renderTab("services", 'Инструменты', <ServicesIcon/>)}
         </header>
     );
 };
