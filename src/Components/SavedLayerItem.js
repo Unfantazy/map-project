67c2bff3b0e4d366ddba44ccfb2ca55c8d3b7b61
Angelina <angelina.kuptcova@gmail.com>
2021-11-05 14:14:09 +0300

    add SavedLayerItem(radioButton)

--- src/Components/SavedLayerItem.js
+++ src/Components/SavedLayerItem.js
@@ -1,14 +1,5 @@
-import {useState} from "react";
-
-
-const SavedLayerItem = ({id}) => {
-    const [isActive, setIsActive] = useState(true);
-
-    const onFilterItemClick = () => {
-        setIsActive(b => !b)
-    }
-
-    return <button className={`FilterItem ${isActive ? 'isActive' : ''}`} onClick={() => onFilterItemClick()}>
+const SavedLayerItem = ({id, handleChange, selectedInput}) => {
+    return <button className={`FilterItem ${selectedInput === id ? 'isActive' : ''}`} onClick={() => handleChange(id)}>
         <span>{id}. Тестовый слой</span>
     </button>
 }
--- src/Components/SavedLayers.js
+++ src/Components/SavedLayers.js
@@ -1,12 +1,19 @@
 import SavedLayerItem from "./SavedLayerItem";
+import {useState} from "react";
 
 
 const SavedLayers = () => {
     const savedLayers = localStorage.getItem('saved_layers');
+    const [selectedInput, setSelectedInput] = useState(0);
+
+    const handleChange = inputValue => {
+        setSelectedInput(inputValue);
+    };
+
     if (savedLayers) {
         const layers = Array.from(JSON.parse(savedLayers))
 
-        return layers.map((layer) => <SavedLayerItem id={layer.id}/>)
+        return layers.map((layer) => <SavedLayerItem id={layer.id} handleChange={handleChange} selectedInput={selectedInput}/>)
     }
 
     return <></>
